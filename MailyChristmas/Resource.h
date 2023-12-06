f72dd99cf41f5ddb12128ed16e4b056730033baa
yel-m <coin6442@gmail.com>
2023-12-06 23:05:57 +0900

    feat : 다이얼로그 UI

--- MailyChristmas/Resource.h
+++ MailyChristmas/Resource.h
@@ -1,21 +1,37 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// MailyChristmas.rc에서 사용
+// MailyChristmas.rc에서 사용되고 있습니다.
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_MAILYCHRISTMAS_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MAILYCHRISTMAS_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_FRO                    1000
+#define IDC_EDIT_USER                   1000
+#define IDC_EDIT_TO                     1001
+#define IDC_EDIT_SUBJECT                1002
+#define IDC_EDIT_BODY                   1003
+#define IDC_EDIT_ATTACHMENTS            1004
+#define IDC_BUTTON_ADD                  1005
+#define IDC_BUTTON_CLEAR                1006
+#define IDC_EDIT_SERVER                 1007
+#define IDC_EDIT_PASSWORD               1010
+#define IDC_EDIT_FROM                   1011
+#define IDC_RADIO_SANTA                 1013
+#define IDC_RADIO_RUDOLF                1014
+#define IDC_COMBO1                      1015
+#define IDC_COMBO_PORT                  1015
+#define IDC_RADIO_SNOWMAN               1016
+#define IDC_RADIO_COOKIE                1017
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
